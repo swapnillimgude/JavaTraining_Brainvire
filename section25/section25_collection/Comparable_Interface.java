a888329382d38e7cb45c752b74278ef542b08778
Swapnil_Limgude <swapnil.limgude@brainvire.com>
2023-09-01 14:17:07 +0530

    section25

--- section25/section25_collection/Comparable_Interface.java
+++ section25/section25_collection/Comparable_Interface.java
@@ -2,34 +2,34 @@ package section25_collection;
 
 import java.util.ArrayList;
 
-class Student implements Comparable <Student>
-{
+class Student implements Comparable<Student> {
 	int rollno;
 	String name;
 	int age;
 
-	public Student(int rn, String nm, int age)
-	{
+	public Student(int rn, String nm, int age) {
 		super();
-		this.rollno=rn;
-		this.name=nm;
-		this.age=age;
+		this.rollno = rn;
+		this.name = nm;
+		this.age = age;
 	}
+
 	@Override
 	public int compareTo(Student o) {
 		// TODO Auto-generated method stub
-		if(age==o.age)
-		return 0;
-		
-		else if(age>o.age)
-		return 1;
-		
+		if (age == o.age)
+			return 0;
+
+		else if (age > o.age)
+			return 1;
+
 		else
-		return -1;
-		
+			return -1;
+
 	}
-	
+
 }
+
 public class Comparable_Interface {
 
 	public static void main(String[] args) {
@@ -38,15 +38,11 @@ public class Comparable_Interface {
 		as.add(new Student(1, "abc", 23));
 		as.add(new Student(2, "pqr", 25));
 		as.add(new Student(3, "xyz", 21));
-		
-		for(Student st : as)
-		{
-			System.out.println(st.rollno+" "+st.name+" "+st.age);
+
+		for (Student st : as) {
+			System.out.println(st.rollno + " " + st.name + " " + st.age);
 		}
-	
-
 		
-
 	}
 
 }
